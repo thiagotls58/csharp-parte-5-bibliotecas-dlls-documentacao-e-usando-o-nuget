1a8606a7c15d340de42b73beb98e2b8657fe799e
Thiago Luz <thiagotls58@gmail.com>
2020-12-22 09:09:29 -0300

    Aulas 1, 2, 3 e 4.

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(847, 489754);
+
+            new ContaCorrente(13, 4568);
+
+
+            FuncionarioAutenticavel carlos = null;
+            carlos.Autenticar("kjhasfkjas");
+
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# csharp-parte-5-bibliotecas-dlls-documentacao-e-usando-o-nuget
- C# parte 5: Bibliotecas DLLs, documentação e usando o NuGet
+# C# parte 5: Bibliotecas DLLs, documentação e usando o NuGet
+
+## Aulas
+1. Projeto da biblioteca
+2. Modificadores de acesso em bibliotecas
+3. Referencia de dll
+4. Documentando
+5. NuGet
